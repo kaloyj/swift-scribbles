e3a11144a0a277cae65cb8a1d7dd360a0cc17b00
Carlo Janea <janeacarlo@gmail.com>
2018-12-26 18:53:46 +0800

    added update to blogs

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -54,5 +54,45 @@ class BlogController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/blog/{blog_id}/update", name="update_blog")
+     */
+    public function update($blog_id)
+    {
+        $blog_repository = $this->getDoctrine()->getRepository(Blog::class);
+        $toUpdate_blog = $blog_repository->find($blog_id);
+        return $this->render('blog/update_blog.html.twig', [
+            'blog' => $toUpdate_blog,
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{blog_id}/update/process", name="process_update_blog")
+     */
+    public function processUpdateBlog(Request $request, $blog_id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $blog_repository = $this->getDoctrine()->getRepository(Blog::class);
+        $toUpdate_blog = $blog_repository->find($blog_id);
+        date_default_timezone_set('Asia/Singapore');
+
+        $toUpdate_blog->setCreationDate(new \DateTime());
+        $toUpdate_blog->setModificationDate(new \DateTime());
+        $toUpdate_blog->setFeaturedPhoto($request->request->get('blogFeaturedPhoto'));
+        $toUpdate_blog->setFeaturedContent($request->request->get('blogFeaturedContent'));
+        $toUpdate_blog->setTitle($request->request->get('blogTitle'));
+        $toUpdate_blog->setContent($request->request->get('blogContent'));
+        $toUpdate_blog->setAuthor($this->getUser());
+
+        $em->persist($toUpdate_blog);
+        $em->flush();
+
+        $blogs = $blog_repository->findBy([], ['modificationDate' => 'DESC']);
+        return $this->render('home/index.html.twig', [
+            'success' => 'Successfully updated blog: '.$toUpdate_blog->getTitle(),
+            'blogs' => $blogs,
+        ]);
+    }
+
 
 }
